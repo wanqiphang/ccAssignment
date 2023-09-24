8a5e779984c5e11045738a35bbd2ad916da131ee
wanqi <90081993+wanqiphang@users.noreply.github.com>
2023-09-25 00:43:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def AddSupervisor():
     email = request.form['email']
     contactNum = request.form['contactNum']
     
-    insert_sql = "INSERT INTO Supervisor VALUES (%s, %s, %s, %s)"
+    insert_sql = "INSERT INTO Supervisor (staffID, name, email, contactNum) VALUES (%s, %s, %s, %s)"
     cursor = db_conn.cursor()
     cursor.execute(insert_sql, (staffID, name, email, contactNum))
     flash('Supervisor Added Successfully')
